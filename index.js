44474f24ec967581bd56d49b44713eebf2091c51
logeshbaskarlb <logeshbaskar007@gmail.com>
2024-01-05 16:44:20 +0530

    login authentication backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const nodemailer = require("nodemailer")
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://heartfelt-truffle-f16fdb.netlify.app",
   })
 );
 
