9e987ac6ac54e090a4d310d67f8bdbf0e7bc413a
logeshbaskarlb <logeshbaskar007@gmail.com>
2024-01-21 14:31:45 +0530

    aaa

--- index.js
+++ index.js
@@ -122,8 +122,9 @@ app.post("/forgot-password", async (req, res) => {
         from: process.env.MAIL_ID,
         to: email,
         subject: 'Reset password link',
-        html: `Click the following link to reset your password: ${process.env.CILENT_URL}/reset-password/${token}`
+        html: `Click the following link to reset your password: https://dapper-otter-b823ba.netlify.app/ /reset-password/${token}`
     });
+    console.log(process.env.CILENT_URL)
    console.log(info);
     res.status(200).json({ message: 'Password reset link sent successfully.' });
 } catch (error) {
